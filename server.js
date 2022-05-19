acbe83b7d9e0f42361f2c8c20cad6ecb501ab2d9
Seba <s.elgueda@outlook.com>
2022-05-19 12:13:17 -0400

    demo

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.get("/", async (req, res) => {
 })
 
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 80
 app.listen(port, () => console.log(`Listening on port ${port}`))
